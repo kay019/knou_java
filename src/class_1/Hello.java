e5801cc5e76d08d833d78a29f32b9fcf1688262c
kim junghoon <crowruya@colosseum.kr>
2022-03-13 18:16:59 +0900

    first update

---
+++ src/class_1/Hello.java
@@ -0,0 +1,13 @@
+package first_1;
+
+/* The Hello class implements an application that
+* simply prints "Hellom Java Applcaion!" to standard output.
+* */
+
+public class Hello{
+    public static void main(String args[]){//main method
+
+        /*Distplay the string*/
+        System.out.println("Hello, Java Application!");
+    }
+}
